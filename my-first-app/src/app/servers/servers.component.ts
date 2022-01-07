f7bf6bed5fdf4ed41cd1f0f50c26ff0a6d33b9e9
Willy Alers <willy.alers@allheart.net>
2022-01-07 11:21:42 -0800

    27 String Interpolation.

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,18 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  templateUrl:  './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
