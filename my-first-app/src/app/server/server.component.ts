64e16fa2408cdaa454d7162f7764aa560a8498b3
Willy Alers <willy.alers@allheart.net>
2022-01-07 18:04:28 -0800

    40 Styling Elements Dynamically with ngStyle.

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -9,7 +9,15 @@ export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = 'offline';
 
+    constructor(){
+        this.serverStatus = Math.random() > .5 ? 'online' : 'offline';
+    }
+
     getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
