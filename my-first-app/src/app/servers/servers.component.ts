88c944593cb6949c11face632dc9ff35c9470c8b
Willy Alers <willy.alers@allheart.net>
2022-01-07 16:32:27 -0800

    35 Combining all Forms of Databinding.

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -20,7 +20,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event:any){
