af131d76ed6cef29ceceb869115521525b5e5c7d
Willy Alers <willy.alers@allheart.net>
2022-01-10 11:25:36 -0800

    55 Outputting a List of Recipes with ngFor.

--- recipe-list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes:Recipe[] = [
+    new Recipe('A Test Recipe','This is simply a test','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg'),
+    new Recipe('A Test Recipe','This is simply a test','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg'),
     new Recipe('A Test Recipe','This is simply a test','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg')
   ];
 
