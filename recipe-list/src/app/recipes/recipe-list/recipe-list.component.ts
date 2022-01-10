253c1c46e9d1b3a090256627a85c8ccdc7c796db
Willy Alers <willy.alers@allheart.net>
2022-01-10 11:20:25 -0800

    54 Adding Content to the Recipes Components.

--- recipe-list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes:Recipe[] = [
+    new Recipe('A Test Recipe','This is simply a test','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg')
+  ];
 
   constructor() { }
 
