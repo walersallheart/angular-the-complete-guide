c2b1f028c7535dee391565d8dd23da800cd807a1
Willy Alers <willy.alers@allheart.net>
2022-01-11 12:09:28 -0800

    108 Injecting the Logging Service Into Components.

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,11 +1,15 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers:[LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService:LoggingService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
@@ -13,6 +17,8 @@ export class NewAccountComponent {
       status: accountStatus
     });
 
-    
+    //loggingService is made automatically by Angular since its listed in the constructor arguments
+    //and the class is provided under "providers"
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
