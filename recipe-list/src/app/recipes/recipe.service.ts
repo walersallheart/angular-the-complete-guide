a1b96e4927dabfeeb0629143f9d9e45adcd45738
Willy Alers <willy.alers@allheart.net>
2022-01-12 15:42:18 -0800

    122 Adding Ingredients to Recipes.

--- recipe-list/src/app/recipes/recipe.service.ts
+++ recipe-list/src/app/recipes/recipe.service.ts
@@ -1,13 +1,27 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes:Recipe[] = [
-        new Recipe('A Test Recipe!','This is simply a test','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg'),
-        new Recipe('Another Test Recipe','This is simply a test','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg'),
-        new Recipe('Yet Another Test Recipe','This is simply a test','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg')
+        new Recipe(
+            'A Test Recipe!',
+            'This is simply a test','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg',
+            [
+                new Ingredient('Meat',1),
+                new Ingredient('French Fries',20)
+            ]
+        ),
+        new Recipe(
+            'Another Test Recipe',
+            'This is simply a test','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg',
+            [
+                new Ingredient('Buns',2),
+                new Ingredient('Meat',1)
+            ]
+        )
     ];
 
     getRecipes() {
