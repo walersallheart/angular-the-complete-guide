93ea4a10a0a35c7b7cfe2eba0b700877c1a99fcd
Willy Alers <willy.alers@allheart.net>
2022-01-12 11:42:34 -0800

    118 Managing Recipes in a Recipe Service.

--- recipe-list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,15 +10,12 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes:Recipe[] = [
-    new Recipe('A Test Recipe','This is simply a test','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg'),
-    new Recipe('Another Test Recipe','This is simply a test','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg'),
-    new Recipe('Yet Another Test Recipe','This is simply a test','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg')
-  ];
+  recipes!:Recipe[];
 
-  constructor() { }
+  constructor(private recipeService:RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe:Recipe)  {
--- recipe-list/src/app/recipes/recipe.service.ts
+++ recipe-list/src/app/recipes/recipe.service.ts
@@ -1,3 +1,14 @@
+import { Recipe } from "./recipe.model";
+
 export class RecipeService{
-    
+    private recipes:Recipe[] = [
+        new Recipe('A Test Recipe!','This is simply a test','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg'),
+        new Recipe('Another Test Recipe','This is simply a test','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg'),
+        new Recipe('Yet Another Test Recipe','This is simply a test','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg')
+    ];
+
+    getRecipes() {
+        //we use slice to return a copy of the array instead of a reference, since its supposed to be private
+        return this.recipes.slice();
+    }
 }
