eb1cb179c7f8b96245f7dc2fae4a4058a8681e72
Willy Alers <willy.alers@allheart.net>
2022-01-12 16:14:08 -0800

    127 Setting up and Loading Routes.

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -11,7 +11,13 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes:Routes = [
+  { path:'', component: HomeComponent },
+  { path:'users', component: UserComponent },
+  { path:'servers', component: ServersComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,6 +31,7 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
