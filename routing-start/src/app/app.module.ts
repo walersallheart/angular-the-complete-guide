eaca11be8e9c9e9d9949d0167834f3b5e9b639b8
Willy Alers <willy.alers@allheart.net>
2022-01-14 15:37:14 -0800

    145 Outsourcing the Route Configuration.

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -11,22 +11,10 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes:Routes = [
-  { path:'', component: HomeComponent },
-  { path:'users', component: UsersComponent, children:[
-    { path:':id/:name', component: UserComponent }
-  ] },
-  { path:'servers', component: ServersComponent, children:[
-    { path:':id', component: ServerComponent },
-    { path:':id/edit', component: EditServerComponent }
-  ]
-  },
-  { path:'not-found', component:PageNotFoundComponent },
-  { path: '**', redirectTo:'not-found' }
-];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -41,7 +29,7 @@ const appRoutes:Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
