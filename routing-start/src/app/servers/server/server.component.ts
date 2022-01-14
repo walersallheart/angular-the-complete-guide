90d5b708d46a4a56302b39ec8105a72982208501
Willy Alers <willy.alers@allheart.net>
2022-01-14 12:06:02 -0800

    142 Configuring the Handling of Query Parameters.

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -11,7 +11,7 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService, private route:ActivatedRoute) { }
+  constructor(private serversService: ServersService, private route:ActivatedRoute, private router:Router) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
@@ -23,6 +23,10 @@ export class ServerComponent implements OnInit {
           this.server = this.serversService.getServer(+params['id']);
         }
       )
+  }
+
+  onEdit() {
+    this.router.navigate(['edit'], { relativeTo: this.route, queryParamsHandling: 'preserve' })
   }
 
 }
