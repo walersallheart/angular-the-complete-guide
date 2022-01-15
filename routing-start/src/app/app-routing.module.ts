13233c242cc577e4709d1cf0b8dc9005f9d0b6a5
Willy Alers <willy.alers@allheart.net>
2022-01-14 16:31:38 -0800

    150 Controlling Navigation with canDeactivate.

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { PageNotFoundComponent } from "./page-not-found/page-not-found.component
 import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
 import { AuthGuard } from "./auth-guard.service";
+import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
  
 const appRoutes: Routes = [
     { path: "", component: HomeComponent },
@@ -25,7 +26,7 @@ const appRoutes: Routes = [
         canActivateChild:[AuthGuard],
         children: [
             { path: ":id", component: ServerComponent },
-            { path: ":id/edit", component: EditServerComponent },
+            { path: ":id/edit", component: EditServerComponent, canDeactivate:[CanDeactivateGuard] },
         ],
     },
     { path: "not-found", component: PageNotFoundComponent },
--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,30 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { ServersService } from '../servers.service';
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
+  changesSaved = false;
 
-  constructor(private serversService: ServersService, private route:ActivatedRoute) { }
+  constructor(private serversService: ServersService,
+              private route:ActivatedRoute,
+              private router:Router) { }
 
   ngOnInit() {
-    //this.route.snapshot.queryParams;
+    this.route.queryParams
+      .subscribe(
+        (queryParams:Params) =>  {
+          this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+        }
+      )
 
-    this.server = this.serversService.getServer(1);
+    this.route.fragment.subscribe();
+    const id = this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.changesSaved = true;
+
+    this.router.navigate(['../'], { relativeTo: this.route });
   }
 
+  canDeactivate() : boolean | Observable<boolean> | Promise<boolean> {
+    if (!this.allowEdit) {
+      return true;
+    }
+
+    if ((this.serverName !== this.server.name || this.serverStatus !== this.server.status) && !this.changesSaved) {
+      return confirm('Do you want to discard the changes?');
+    } else {
+      return true;
+    }
+  }
 }
