0796240c43c210bad1d324ba0758793ed085f07e
Willy Alers <willy.alers@allheart.net>
2022-01-17 13:23:37 -0800

    203: Reactive Setup

--- forms-reactive-start/src/app/app.module.ts
+++ forms-reactive-start/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
