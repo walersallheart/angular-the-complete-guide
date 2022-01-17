e71585d8529cbca647ab9455336e54afc1864972
Willy Alers <willy.alers@allheart.net>
2022-01-17 12:24:23 -0800

    200 TD: Using Form Data.

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username:'',
+    email:'',
+    secretQuestion:'',
+    answer:'',
+    gender:''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -37,6 +45,12 @@ export class AppComponent {
   // }
 
   onSubmit(){
+    this.submitted = true;
     console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
