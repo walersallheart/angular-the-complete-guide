a646d96ce1e74cf8529df1c44f4d10fa5aafed28
Willy Alers <willy.alers@allheart.net>
2022-01-20 15:44:44 -0800

    282 Storing Recipes.

--- recipe-list/src/app/header/header.component.ts
+++ recipe-list/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { DataStorageService } from "../shared/data-storage.service";
 
 @Component({
     selector:'app-header',
@@ -6,5 +7,13 @@ import { Component } from "@angular/core";
 })
 
 export class HeaderComponent {
-    
+    constructor(private dataStorageService:DataStorageService){}
+
+    onSaveData(){
+        this.dataStorageService.storeRecipes();
+    }
+
+    onFetchData(){
+
+    }
 }
--- recipe-list/src/app/shared/data-storage.service.ts
+++ recipe-list/src/app/shared/data-storage.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({ providedIn: 'root' })
 
 export class DataStorageService {
-    constructor(private http:HttpClient) {}
+    url = 'https://ng-course-recipe-book-ea2f0-default-rtdb.firebaseio.com/recipes.json';
+
+    constructor(private http:HttpClient,
+                private recipeService:RecipeService) {}
+
+    storeRecipes(){
+        const recipes = this.recipeService.getRecipes();
+
+        this.http
+            .put(
+                this.url,
+                recipes
+            )
+            .subscribe(response => {
+                console.log(response);
+            });
+    }
 }
