2f701c87531e491a6ef85caa68d9b377671998df
Willy Alers <willy.alers@allheart.net>
2022-01-20 11:29:49 -0800

    270 Adding Query Params.

--- http-01-start/src/app/posts.service.ts
+++ http-01-start/src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient,  HttpHeaders } from "@angular/common/http";
+import { HttpClient,  HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
 import { map, catchError } from "rxjs/operators";
@@ -28,12 +28,19 @@ export class PostsService{
     }
 
     fetchPosts() {
+        let searchParams = new HttpParams();
+        //HttpParams is immutable, so you need to overwrite it when you make a chance
+        searchParams = searchParams.append('print', 'pretty');
+        searchParams = searchParams.append('custom1', 'value1');
+        searchParams = searchParams.append('custom2', 'value2');
+
         return this.http.get<{ [key:string] : Post }>(
             this.url,
             {
                 headers: new HttpHeaders({
                     'Custom-Header': 'Hello'
-                })
+                }),
+                params: searchParams
             }
             )
             .pipe(map((responseData) => {
