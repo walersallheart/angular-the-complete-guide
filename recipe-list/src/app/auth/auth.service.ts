027c3b446bcc342b7d8e7402dd5a2a1f4912b043
Willy Alers <willy.alers@allheart.net>
2022-01-22 13:32:57 -0800

    305 Adding Auto-Login.

--- recipe-list/src/app/auth/auth.service.ts
+++ recipe-list/src/app/auth/auth.service.ts
@@ -52,7 +52,28 @@ export class AuthService {
         }));
     }
 
+    autoLogin(){
+        const userData: {
+            email:string;
+            id:string;
+            _token:string;
+            _tokenExpirationDate:string;
+        } = JSON.parse(localStorage.getItem('userData')!);
+
+        if (!userData) {
+            return;
+        }
+
+
+        const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+        if (loadedUser.token){
+            this.user.next(loadedUser);
+        }
+    }
+
     logout(){
+        localStorage.removeItem('userData')
         this.user.next(null!);
         this.router.navigate(['/auth']);
     }
@@ -67,6 +88,7 @@ export class AuthService {
                     );
 
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorResponse:HttpErrorResponse) {
