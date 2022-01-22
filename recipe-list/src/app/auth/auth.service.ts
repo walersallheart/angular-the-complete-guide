6bfb190783ffde6045e6e79a5754b7ece8bd1c03
Willy Alers <willy.alers@allheart.net>
2022-01-22 12:21:13 -0800

    299 Login Error Handling.

--- recipe-list/src/app/auth/auth.service.ts
+++ recipe-list/src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
@@ -27,21 +27,7 @@ export class AuthService {
                 password:password,
                 returnSecureToken:true
             }
-        ).pipe(catchError(errorResponse => {
-            let errorMessage = "An error has occurred";
-
-            if (!errorResponse.error || !errorResponse.error.error) {
-                return throwError(errorMessage);
-            }
-
-            switch(errorResponse.error.error.message) {
-                case 'EMAIL_EXISTS':
-                    errorMessage = 'This email already exists';
-                    break;
-            }
-
-            return throwError(errorMessage);
-        }));
+        ).pipe(catchError(this.handleError));
     }
 
     login(email:string, password:string) {
@@ -52,6 +38,28 @@ export class AuthService {
                 password:password,
                 returnSecureToken:true
             }
-        )
+        ).pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorResponse:HttpErrorResponse) {
+        let errorMessage = "An error has occurred";
+
+        if (!errorResponse.error || !errorResponse.error.error) {
+            return throwError(errorMessage);
+        }
+
+        switch(errorResponse.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'This email already exists';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'Invalid password';
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'Email not found';
+                break;
+        }
+
+        return throwError(errorMessage);
     }
 }
