f44247e2c1b8c873c559f7e1c73748baf75bdffd
Willy Alers <willy.alers@allheart.net>
2022-01-22 12:42:11 -0800

    301 Reflecting the Auth State in the UI.

--- recipe-list/src/app/auth/auth.component.ts
+++ recipe-list/src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthResponseData, AuthService } from "./auth.service";
 
@@ -13,7 +14,7 @@ export class AuthComponent {
     isLoading = false;
     error:string = null!;
 
-    constructor(private authService:AuthService) {}
+    constructor(private authService:AuthService, private router:Router) {}
 
     onSwitchMode(){
         this.isLoginMode = !this.isLoginMode;
@@ -42,6 +43,7 @@ export class AuthComponent {
                 responseData => {
                     console.log(responseData);
                     this.isLoading = false;
+                    this.router.navigate(['/recipes']);
                 },
                 errorMessage => {
                     this.error = errorMessage;
--- recipe-list/src/app/header/header.component.ts
+++ recipe-list/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { AuthService } from "../auth/auth.service";
 import { DataStorageService } from "../shared/data-storage.service";
 
 @Component({
@@ -6,8 +8,17 @@ import { DataStorageService } from "../shared/data-storage.service";
     templateUrl:'./header.component.html'
 })
 
-export class HeaderComponent {
-    constructor(private dataStorageService:DataStorageService){}
+export class HeaderComponent implements OnInit, OnDestroy {
+    isAuthenticated = false;
+    private userSub!:Subscription;
+
+    constructor(private dataStorageService:DataStorageService, private authService:AuthService){}
+
+    ngOnInit(): void {
+        this.userSub = this.authService.user.subscribe(user => {
+            this.isAuthenticated = !!user;
+        });
+    }
 
     onSaveData(){
         this.dataStorageService.storeRecipes();
@@ -15,5 +26,9 @@ export class HeaderComponent {
 
     onFetchData(){
         this.dataStorageService.fetchRecipes().subscribe();
+    }
+
+    ngOnDestroy(): void {
+        this.userSub.unsubscribe();
     }
 }
