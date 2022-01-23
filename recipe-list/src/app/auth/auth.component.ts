cf20c4ac6cdd6329fa2bd8ae48047f36ebc1cc45
Willy Alers <willy.alers@allheart.net>
2022-01-23 12:05:40 -0800

    314 Preparing Programmatic Creation.

--- recipe-list/src/app/auth/auth.component.ts
+++ recipe-list/src/app/auth/auth.component.ts
@@ -1,8 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, ComponentFactoryResolver } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthResponseData, AuthService } from "./auth.service";
+
+import { AlertComponent } from "../shared/alert/alert.component";
 
 @Component({
     selector: 'app-auth',
@@ -14,7 +16,7 @@ export class AuthComponent {
     isLoading = false;
     error:any = null;
 
-    constructor(private authService:AuthService, private router:Router) {}
+    constructor(private authService:AuthService, private router:Router, private componentFactoryResolver:ComponentFactoryResolver) {}
 
     onSwitchMode(){
         this.isLoginMode = !this.isLoginMode;
@@ -47,6 +49,7 @@ export class AuthComponent {
                 },
                 errorMessage => {
                     this.error = errorMessage;
+                    this.showErrorAlert(errorMessage);
                     this.isLoading = false;
                 }
             );
@@ -56,5 +59,11 @@ export class AuthComponent {
 
     onHandleError(){
         this.error = null;
+    }
+
+    private showErrorAlert(message:string){
+        this.componentFactoryResolver.resolveComponentFactory(
+            AlertComponent
+        );
     }
 }
