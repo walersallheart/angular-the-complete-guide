022ba9c7d6402a66ad08e11db6150dcad5c352a2
Willy Alers <willy.alers@allheart.net>
2022-01-25 10:52:32 -0800

    355 Preparing Update & Delete Actions.

--- recipe-list/src/app/shopping-list/store/shopping-list.actions.ts
+++ recipe-list/src/app/shopping-list/store/shopping-list.actions.ts
@@ -3,6 +3,8 @@ import { Ingredient } from "src/app/shared/ingredient.model";
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
 
 export class AddIngredient implements Action {
     readonly type: string = ADD_INGREDIENT;
@@ -14,4 +16,18 @@ export class AddIngredients implements Action {
     constructor(public payload:Ingredient[]){}
 }
 
-export type ShoppingListActions = AddIngredient | AddIngredients;
+export class UpdateIngredient implements Action {
+    readonly type: string = UPDATE_INGREDIENT;
+    constructor(public payload: { index:number, newIngredient:Ingredient }){}
+}
+
+export class DeleteIngredient implements Action {
+    readonly type: string = DELETE_INGREDIENT;
+    constructor(public payload: number){}
+}
+
+export type ShoppingListActions =
+    AddIngredient |
+    AddIngredients |
+    UpdateIngredient |
+    DeleteIngredient;
