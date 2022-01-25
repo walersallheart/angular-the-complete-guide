6e1bb0713628cd52b3b94eff6188ca03d32697e5
Willy Alers <willy.alers@allheart.net>
2022-01-24 21:11:56 -0800

    348 Getting Started with Reducers.

---
+++ recipe-list/src/app/shopping-list/shopping-list.reducer.ts
@@ -0,0 +1,13 @@
+import { Action, State } from "@ngrx/store";
+import { Ingredient } from "../shared/ingredient.model";
+
+const initialState = {
+    ingredients : [
+        new Ingredient('Apples',5),
+        new Ingredient('Tomatoes',10)
+    ]
+};
+
+export function shoppingListReducer(state = initialState, action:Action){
+
+}
