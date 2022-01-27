f91821bee7b7951330b19058b69fd5d4bd6dacbd
Willy Alers <willy.alers@allheart.net>
2022-01-27 10:59:18 -0800

    409 Transitions.

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { state, style, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -13,7 +13,9 @@ import { Component } from '@angular/core';
       state('highlighted', style({
         'background-color':'blue',
         transform:'translateX(100px)'
-      }))
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800))
     ])
   ]
 })
